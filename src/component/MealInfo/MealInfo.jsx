a69cf7edbacf1a3b72e34606a2192ad4fb50debe
tanisha1527 <vermatanisha620@gmail.com>
2025-01-06 16:36:01 +0530

    Add mealInfo  using meal db API and edit some css propertise

--- src/component/MealInfo/MealInfo.jsx
+++ src/component/MealInfo/MealInfo.jsx
@@ -1,19 +1,40 @@
 import React, { useState } from 'react'
 import './MealInfo.css'
+import { useParams } from 'react-router-dom'
 
 const MealInfo = () => {
-    
-  return (
-    <div className='meal-info'>
-        <img src="" alt="" />
-        <div className='info'>
-            <h1>Recipe Detail</h1>
-            <button></button>
-            <h3>Instructions</h3>
-            <p></p>
-        </div>
-    </div>
-  )
-}
 
-export default MealInfo
+  const {mealid} = useParams();
+  const [info,setInfo] = useState();
+  console.log(mealid)
+
+  const getInfo = async () => {
+
+       const get = await fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealid}`);
+       const jsonData = await get.json();
+       console.log(jsonData.meals[0]);
+       setInfo(jsonData.meals[0]);
+  }
+  if (info != ""){
+      getInfo();            
+  }
+  return (
+   <div>
+    {
+      !info ? "Data Not Found" : 
+      <div className='meal-info'>
+      <img src={info.strMealThumb} />
+      <div className='info'>
+          <h1>Recipe Detail</h1>
+          <button>{info.strMeal}</button>
+          <h3>Instruction's</h3>
+          <p>{info.strInstructions}</p>
+      </div>
+    </div>
+    }
+  </div>
+  )
+    
+  }
+
+export default MealInfo 
