7e53e147baed192153ec75c56c82270bd03d2544
tanisha1527 <vermatanisha620@gmail.com>
2025-01-06 17:01:13 +0530

    Add heading and update code

--- src/component/MainPage/Mainpage.jsx
+++ src/component/MainPage/Mainpage.jsx
@@ -5,30 +5,36 @@ import MealCards from '../MealCards/MealCards';
 const Mainpage = () => {
       
       const [data,setData] = useState();
-      const [search,setSearch] = useState();
+      const [search,setSearch] = useState("");
+      const [msg,setMsg] = useState("");
       
       const handleInput = (e) =>{
            setSearch(e.target.value)
       }
 
        const myFood = async () => {
+         if (search == ""){
+            setMsg("Please Enter Something...");
+         }
+         else{
+            const get = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${search}`);
+       const jsonData = await get.json();
+       setData(jsonData.meals);
+       setMsg("");
+      
+      }
 
-       const get = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${search}`);
-       const jsonData = await get.json()
-      // console.log(jsonData.meals);
-       setData(jsonData.meals)
   }
-
- // console.log(data);
 
   return (
     <>
+       <h1 className='heading'>FOOD RECIPE APP</h1>
        <div className="container">
            <div className="search-bar">
               <input type="text" placeholder='Enter Dishes' onChange={handleInput} />
               <button onClick={myFood}>Search</button>
            </div>
-
+            <h4 className='msg'>{msg}</h4>
            <div>
               <MealCards detail={data}/>
            </div>
